597d86babefbe19b3a3c71fc70cdda9c14eca931
Private Chen <chenbbml@gmail.com>
2018-10-09 00:16:17 +0800

    Commit_20181009
    add jquery and vue

--- demo/src/main/resources/static/script_home.js
+++ demo/src/main/resources/static/script_home.js
@@ -1,28 +1,26 @@
-function getAllUsers(){
-    var xmlhttp;
-    if (window.XMLHttpRequest)
-    {
-        //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-        xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {
-        // IE6, IE5 浏览器执行代码
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
+$(document).ready(function(){
+    $("#query_button").click(function(){
+        $.get(
+            "http://localhost:8080/users",
+            function(data,status){
+                    var json=JSON.parse(data);
+                    json.forEach(
+                        function(value,index,array){
+                            var innerHTML="<h2>"+value.id+"</h2></br><h2>"+value.username+"</h2></br><h2>"+value.password+"</h2></br>";
+                            $("#div_result").append(innerHTML);
+                        });
+        });
+    });
 
-    xmlhttp.onreadystatechange=function()
-    {
-        if(xmlhttp.readyState==4&&xmlhttp.status==200){
-            var json=JSON.parse(xmlhttp.responseText);
-            json.forEach(
-                function(value,index,array){
-                    var innerHTML="<h2>"+value.id+"</h2></br><h2>"+value.username+"</h2></br><h2>"+value.password+"</h2></br>";
-                    document.getElementById("div_result").innerHTML+=innerHTML;
-                });
+    $("#mail").bind('keypress',function(event){
+        if(event.keyCode == "13")
+        {
+            import loadingComponent from './loading.vue';
+            const loading={
+                install:function(Vue){
+                    Vue.component('Loading',loadingComponent);
+            },
+            export default loading
         }
-
-    }
-    xmlhttp.open("GET","http://localhost:8080/users",true);
-    xmlhttp.send(null);
-}
+    });
+});
