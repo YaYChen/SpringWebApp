0ef9590036b58ebbe0681678518d63283d638c2a
Private Chen <chenbbml@gmail.com>
2018-10-12 00:28:08 +0800

    Commit_20181012
    add nuxt and vue.js

---
+++ webapp/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="layout_main">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.layout_main{
+    width: 100%;
+    margin: 0 auto;
+    height: 100%;
+}
+</style>
